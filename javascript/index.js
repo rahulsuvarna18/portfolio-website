f16478b2e8c665fdde2efaca8544e0ebe2786673
Rahul Suvarna <rahulsuvarna18@gmail.com>
2023-10-15 11:47:28 +0200

    Defined font styles, line height and colors for the website

--- javascript/index.js
+++ javascript/index.js
@@ -1,20 +1,20 @@
-"use strict";
-const contactButton = document.querySelector(".contact-button");
+// "use strict";
+// const contactButton = document.querySelector(".contact-button");
 
-function handleClick() {
-  console.log("Clikced");
+// function handleClick() {
+//   console.log("Clikced");
 
-  const contactForm = document.querySelector(".contact-form");
+//   const contactForm = document.querySelector(".contact-form");
 
-  contactForm.classList.remove("hidden");
-  setTimeout(() => {
-    console.log("Delayed for 1 second.");
-    contactForm.style.opacity = "1";
-    // contactForm.style.transform = "translateY(20px)";
-  }, "1");
+//   contactForm.classList.remove("hidden");
+//   setTimeout(() => {
+//     console.log("Delayed for 1 second.");
+//     contactForm.style.opacity = "1";
+//     // contactForm.style.transform = "translateY(20px)";
+//   }, "1");
 
-  // contactForm.textContent = "Hi";
-  // document.querySelector(".contact-form").style.display = "grid";
-}
+//   // contactForm.textContent = "Hi";
+//   // document.querySelector(".contact-form").style.display = "grid";
+// }
 
-contactButton.addEventListener("click", handleClick);
+// contactButton.addEventListener("click", handleClick);
